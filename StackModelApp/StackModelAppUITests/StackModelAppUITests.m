e0511a44e5c530585fa9cbdc1561cf944c1d46aa
Akshay Acharya <akshay.acharya@capgemini.com>
2016-08-10 11:48:59 +0530

    Added UI test cases

--- StackModelApp/StackModelAppUITests/StackModelAppUITests.m
+++ StackModelApp/StackModelAppUITests/StackModelAppUITests.m
@@ -22,7 +22,81 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
     [[[XCUIApplication alloc] init] launch];
+    XCUIElement *stackModelNavigationBar = app.navigationBars[@"Stack Model"];
+    XCUIElement *addButton = stackModelNavigationBar.buttons[@"Add"];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    [addButton tap];
+    
+    XCUIElement *minusButton = stackModelNavigationBar.buttons[@"minus"];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [minusButton tap];
+    [[app.otherElements containingType:XCUIElementTypeAlert identifier:@"Error"].element tap];
+    [app.alerts[@"Error"].collectionViews.buttons[@"Ok"] tap];
+    
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
